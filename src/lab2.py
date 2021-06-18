ad83fa84af3265939bfdb3a479bf3f7b898384e7
paopaowu <897873227@qq.com>
2021-06-18 16:09:29 +0800

    Update lab2.py

--- src/lab2.py
+++ src/lab2.py
@@ -3,12 +3,15 @@ class Variable :
         self.v=v
     def FV(self):
         return self.v
+    def BV(self):
+        return list()
 class Constant :
     def __init__(self,c):
         self.c=c
     def FV(self):
-        return None
-
+        return list()
+    def BV(self):
+        return list()
 class Combination :
     def __init__(self,s,t):
         self.s=s
@@ -25,19 +28,34 @@ class Combination :
             temp=temp+self.s.FV()
 
         return temp
+    def BV(self):
+        temp=[]
+        if isinstance(self.s.BV(), String):
+            temp.append(self.s.BV())
+        else:
+            temp=temp+self.s.BV()
+        if isinstance(self.t.BV(), String):
+            temp.append(self.t.BV())
+        else:
+            temp=temp+self.s.BV()
+
+        return temp
 
 class Abstractions :
     def __init__(self,x,s):
         self.x=x
         self.s=s
     def FV(self):
-        list=[]
-        if temp==None:
-            return None
+        temp=[]
         if isinstance(self.s.FV(),String):
-            list.append(self.s.FV())
+            temp.append(self.s.FV())
         else:
-            list=self.s.FV()
-        list.remove(self.x)
-        return  list
+            temp=self.s.FV()
+        temp.remove(self.x)
+        return  temp
 
+    def BV(self):
+        temp=[]
+        temp=temp+self.s.BV()
+        temp.append(self.x)
+        return temp
